4551cfeb8ba675e184368e2ff1e2208ebf2df0ce
Amir_Iz <amirizadnia2000@gmail.com>
2025-09-12 16:28:00 +0330

    common update

--- Socket/Room/Commands/Base/GameCommand.cs
+++ Socket/Room/Commands/Base/GameCommand.cs
@@ -4,7 +4,6 @@ public class GameCommand : IGameCommand
 {
     public GameCommandType Type { get; set; }
     public string UserId { get; set; }
-
     public int PlayerIndex { get; set; }
 
     public T GetSubType<T>() where T : GameCommand
--- Socket/Room/RoomSocketPacketType.cs
+++ Socket/Room/RoomSocketPacketType.cs
@@ -2,6 +2,8 @@ namespace PaeezanAssignment_Server.Common.Socket;
 
 public enum RoomSocketPacketType
 {
+    PI, // ping
+    PO, // pong
     C, // connect 
     D, // disconnect
     M  // message
