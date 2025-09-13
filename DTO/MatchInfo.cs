3d819acd6e97320e0ea605f0e7735041cc08a1d5
Amir_Iz <amirizadnia2000@gmail.com>
2025-09-13 12:48:28 +0330

    update

---
+++ DTO/MatchInfo.cs
@@ -0,0 +1,8 @@
+namespace Common.DTO;
+
+public class MatchInfo
+{
+    public Guid Adress { get; set; }
+    public int ParticipantCount { get; set; }
+    public List<string> participants { get; set; }
+}
