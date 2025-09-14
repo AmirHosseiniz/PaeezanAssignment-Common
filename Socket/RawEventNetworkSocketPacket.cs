6754cba863e5ab0aaeed4c0546cf7e55b5d526fe
Amir_Iz <amirizadnia2000@gmail.com>
2025-09-14 14:39:16 +0330

    testing

--- Socket/RawEventNetworkSocketPacket.cs
+++ Socket/RawEventNetworkSocketPacket.cs
@@ -3,9 +3,9 @@ namespace Common.Socket
     public class RawEventNetworkSocketPacket
     {
         public string UserId { get; set; }
-        public dynamic? Data { get; set; }
+        public string Data { get; set; }
 
-        public RawEventNetworkSocketPacket(string userId, dynamic? data)
+        public RawEventNetworkSocketPacket(string userId, string data)
         {
             UserId = userId;
             Data = data;
