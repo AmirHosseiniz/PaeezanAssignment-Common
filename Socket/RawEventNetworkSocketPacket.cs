867f13030ca2d2f7d1a4c09ea73413322a71430b
Amir_Iz <amirizadnia2000@gmail.com>
2025-09-14 13:11:29 +0330

    common

--- Socket/RawEventNetworkSocketPacket.cs
+++ Socket/RawEventNetworkSocketPacket.cs
@@ -3,9 +3,9 @@ namespace Common.Socket
     public class RawEventNetworkSocketPacket
     {
         public string UserId { get; set; }
-        public string Data { get; set; }
+        public dynamic? Data { get; set; }
 
-        public RawEventNetworkSocketPacket(string userId, string data)
+        public RawEventNetworkSocketPacket(string userId, dynamic? data)
         {
             UserId = userId;
             Data = data;
